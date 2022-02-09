5009336096b7d612100f748feb4144b37b2bb216
Wilmer <terabytesoftw@gmail.com>
2022-02-09 17:26:03 -0300

    add method `insert()`.

--- src/PDO/SchemaPDOMysql.php
+++ src/PDO/SchemaPDOMysql.php
@@ -488,6 +488,36 @@ final class SchemaPDOMysql extends Schema
         return $sql;
     }
 
+    public function insert(string $table, array $columns): bool|array
+    {
+        $command = $this->db->createCommand()->insert($table, $columns);
+        $tablePrimaryKey = [];
+
+        if (!$command->execute()) {
+            return false;
+        }
+
+        $tableSchema = $this->getTableSchema($table);
+        $result = [];
+
+        if ($tableSchema !== null) {
+            $tablePrimaryKey = $tableSchema->getPrimaryKey();
+        }
+
+        /** @var string $name */
+        foreach ($tablePrimaryKey as $name) {
+            if ($tableSchema?->getColumn($name)?->isAutoIncrement()) {
+                $result[$name] = $this->getLastInsertID((string) $tableSchema?->getSequenceName());
+                break;
+            }
+
+            /** @var mixed */
+            $result[$name] = $columns[$name] ?? $tableSchema?->getColumn($name)?->getDefaultValue();
+        }
+
+        return $result;
+    }
+
     /**
      * Loads the column information into a {@see ColumnSchema} object.
      *
